0bf67e55afb19e1ac0a6a75f8688dee7238f62d4
SravaniBhumireddy <sravanivengalareddy2004@gmail.com>
2025-12-01 11:12:39 +0530

    Made updates

--- main.js
+++ main.js
@@ -66,10 +66,10 @@ console.log(todoComplete);
 */
 
 /*
-const x=4;
+const x=6;
 const y=10;
 
-if(x >5 || y >10){
+if(x >5 && y >10){
     console.log("x is 10");
 }
 else if(x>10){
@@ -79,7 +79,6 @@ else {
     console.log('x is not 10');
 }
 
-*/
 
 const x=11;
 //const color=x>10 ? 'red' :'blue';
@@ -95,5 +94,10 @@ switch(color){
         console.log('color is not red or blue');
         break;
 }
+*/
 
+function addNums(num1,num2){
+    console.log(num1+num2);
+}
 
+addNums(5,4);
